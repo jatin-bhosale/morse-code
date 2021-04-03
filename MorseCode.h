e6cca3849359a8bb872c7a85bacb16269c37489f
jatin-bhosale <60322040+jatin-bhosale@users.noreply.github.com>
2021-04-04 00:16:19 +0530

    .

--- MorseCode.h
+++ MorseCode.h
@@ -4,19 +4,18 @@
 
 #ifndef MorseCode_h
 #define MorseCode_h
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#include "Arduino.h"
+#include <stdint.h>
 
-
-class MorseCode
-{
-    public:
-        int autoSQL(unsigned int pin);
-        String morseRx(unsigned int pin, int tim, int sql, double tol);
-        void morseTx(unsigned int pin, String ip, int t);
-    private:
-        String morseEncode(String ip);
-        String morseDecode(String ip);
+extern int autoSQL(unsigned int pin);
+extern String morseRx(unsigned int pin, int tim, int sql, double tol);
+extern void morseTx(unsigned int pin, String ip, int t);
+String morseEncode(String ip);
+String morseDecode(String ip);
+#ifdef __cplusplus
 }
-
+#endif
 #endif
