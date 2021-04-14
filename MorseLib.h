f0d1ef7ffaf473d2e1fa5e4aead7767ea39b86f8
Jatin Bhosale <jatin.bhosale@somaiya.edu>
2021-04-14 15:09:17 +0530

    Update MorseLib.h

--- MorseLib.h
+++ MorseLib.h
@@ -9,7 +9,7 @@
 
 extern int autoSQL(unsigned int pin);
 extern String morseRx(unsigned int pin, int tim, int sql, double tol);
-extern  void morseTx(unsigned int pin, String ip, int t);
+extern void morseTx(unsigned int pin, String ip, int t);
 String morseEncode(String ip);
 String morseDecode(String ip);
 String autoCorr(String ip);
